e8f326930201042796822f7c87d4e5e2c78daa26
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-12-22 23:56:42 +0600

    1626A - Equidistant Letters solved successfully

---
+++ 1626A - Equidistant Letters.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+main()
+
+{
+
+    int t;
+
+    cin >> t;
+
+    while (t--)
+
+    {
+
+        string s;
+
+        cin >> s;
+
+        sort(s.begin(), s.end());
+
+        cout << s << endl;
+    }
+}
