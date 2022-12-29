30732aca16149f4408f5b63a8d5759141d611af5
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-12-29 09:58:17 +0600

    1550A - Find The Array solved successfully

---
+++ 1550A - Find The Array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+main()
+
+{
+
+    int t, n;
+
+    cin >> t;
+
+    while (t--)
+
+    {
+
+        cin >> n;
+
+        cout << ceil(sqrt(n)) << endl;
+    }
+}
