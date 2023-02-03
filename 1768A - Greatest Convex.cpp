c03fd85ce9ca37ef04143e96e9bc3f96ed52c714
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-02-03 22:08:59 +0600

    1768A - Greatest Convex solved successfully

---
+++ 1768A - Greatest Convex.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int tttt;
+    cin >> tttt;
+    while (tttt > 0)
+    {
+        int ghghhg;
+        cin >> ghghhg;
+
+        cout << ghghhg - 1 << endl;
+        tttt--;
+    }
+}
