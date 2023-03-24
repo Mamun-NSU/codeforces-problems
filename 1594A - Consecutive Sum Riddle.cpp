ce565d088daf2f53da94a600d7a6e75e95ec8936
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-03-24 22:54:04 +0600

    1594A - Consecutive Sum Riddle solved successfully

---
+++ 1594A - Consecutive Sum Riddle.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        long long n;
+        cin >> n;
+        // view the image in this repository for better understanding
+        // since negative numbers are included, we can simply include the negative
+        // equivalent of the numbers in the range from 'l' to 'r' (inclusive) and not
+        // include the negative equivalent of 'n' so that every pair would cancel each
+        // other out and leave 'n' behind as the final answer
+        cout << -(n - 1) << " " << n << '\n';
+    }
+    return 0;
+}
