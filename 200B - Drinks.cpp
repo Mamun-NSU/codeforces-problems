00001286627392e79f1c5cbd306987f99b6a7e0f
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-04-17 23:59:31 +0600

    1154A - Restoring Three Numbers solved successfully

---
+++ 200B - Drinks.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double c, b, d = 0;
+    int a;
+
+    scanf("%d", &a);
+    c = a * 100.0;
+    for (; a; a--)
+    {
+        scanf("%lf", &b);
+        d = d + b;
+    }
+
+    printf("%.12f", d / c * 100.0);
+
+    return 0;
+}
