5666e7a29137628593d560ad65b4b19203d20644
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-04-22 23:55:52 +0600

    1535A - Fair Playoff solved successfully

---
+++ 1535A - Fair Playoff.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int a, b, c, d;
+
+        cin >> a >> b >> c >> d;
+        if ((max(a, b) < min(c, d)) || (min(a, b) > max(c, d)))
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
